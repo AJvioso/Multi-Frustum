ab4e360eeaee0f2b93661392f3961fda8c51e67a
JerbiAhmed <jerbi@vioso.com>
2024-06-07 10:23:28 +0200

    add navigation and restart UI

--- Assets/Scripts/UIButtons.cs
+++ Assets/Scripts/UIButtons.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIButtons : MonoBehaviour
 {
     /// <summary>
-    /// Toggle function used by UI buttons
+    /// Toggle function used by UI buttons for several objects
     /// </summary>
     /// <param name="elementToToggle"></param>
     public void ToggleObject(GameObject elementToToggle)
@@ -13,8 +14,21 @@ public class UIButtons : MonoBehaviour
         elementToToggle.SetActive(!elementToToggle.activeSelf);
     }
 
+    /// <summary>
+    /// shut down the app
+    /// </summary>
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+
+    /// <summary>
+    /// RestartButton reloads the current scene
+    /// </summary>
+    public void RestartButton()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
